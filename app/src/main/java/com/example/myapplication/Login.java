7874380e46a1fc987a99aae88aed30a053edffb0
Kavinduweb <kavindupro40@gmail.com>
2021-09-23 13:02:47 +0530

    login page

---
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    private Button logbtn;
+    private EditText email , pass ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+        logbtn = findViewById(R.id.log_btn);
+        email = findViewById(R.id.emaillog);
+        pass = (EditText)findViewById(R.id.log_pass);
+
+        logbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+
+
+    }
+
+    public void login(){
+
+
+        if(email.getText().toString().equals("admin") && pass.getText().toString().equals("admin"))
+         {
+            Intent intent = new Intent(this, PaymentHome.class);
+            startActivity(intent);
+
+        }
+        else{
+            Toast toast = Toast.makeText(this,"Login Fail",Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+}
