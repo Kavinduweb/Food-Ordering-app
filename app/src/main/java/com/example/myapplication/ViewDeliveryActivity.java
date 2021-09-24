ae5aeb6bb4e2e4571e0e20cc411b9e3c0945d133
vajira-priyangani <it20107000@my.sliit.lk>
2021-09-24 21:29:49 +0530

    Diliver Main Activity uploded

---
+++ app/src/main/java/com/example/myapplication/ViewDeliveryActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ViewDeliveryActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_delivery);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        DatabaseDeliveryClass databaseDeliveryClass = new DatabaseDeliveryClass(this);
+        List<DeliveryModelClass> deliveryModelClasses = databaseDeliveryClass.getDeliveryList();
+
+        if (deliveryModelClasses.size() > 0){
+            DeliveryAdapterClass deliveryadapterclass = new DeliveryAdapterClass(deliveryModelClasses, ViewDeliveryActivity.this);
+            recyclerView.setAdapter(deliveryadapterclass);
+        }else {
+            Toast.makeText(this, "There is no delivery details in the database", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+    }
+}
